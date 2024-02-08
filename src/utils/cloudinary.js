763e923cbaaefcbc88c76385fbed6d16b854f136
070Pushkar <111866894+070Pushkar@users.noreply.github.com>
2024-02-08 22:46:00 +0530

    new file added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2  as cloudinary} from "cloudinary";
+
+
+import fs  from "fs";
+
+
+          
+cloudinary.config({ 
+  cloud_name: 'backend series', 
+  api_key: '341544326934265', 
+  api_secret: '1RCFKpRUBbAnLYdhQBP6fsQ3g88' 
+});
+
+ 
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+
+
+export {uploadOnCloudinary};
